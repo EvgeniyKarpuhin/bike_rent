ec609b19cf7a93cd01d8d0966ce6b231cde53a2e
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2022-02-12 02:55:32 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Rent from "./Component/rentInform";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="rentInform">
+        <h1>Awesome Bike Rental</h1>
+        <h2>Create new Rent</h2>
+        <div className="rentInput">
+          <span className="rentInput__type">Bike name
+            <input className="rentInput__name"></input>
+          </span>
+          <span className="rentInput__type">Bike type
+          <select className="rentInput__bike">
+            <option>Custom</option>
+            <option>Road</option>
+            <option>Mountain</option>
+          </select>
+          </span>
+          <span className="rentInput__type">Rent Price
+            <input className="rentInput__price"></input>
+            </span>
+          
+          <button className="rentInput__btn" type="submit">Submit rent</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Component/rentInform.js
@@ -0,0 +1,23 @@
+// import React, { Component } from 'react';
+
+// class Rent extends Component {
+//     render() {
+//         return (
+//             <div className='rentInform'>
+//                 <h2>Create new Rent</h2>
+//                 <div className='rentInput'>
+//                     <input></input>
+//                     <select>
+//                         <option>Custom</option>
+//                         <option>Road</option>
+//                         <option>Mountain</option>
+//                     </select>
+//                     <input></input>
+//                     <button>Submit rent</button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Rent;
